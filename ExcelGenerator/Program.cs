672edada1572caa5b3436cc8b2cb9f514207b9ee
nevena-angelova <nev.angelova@gmail.com>
2017-06-29 11:18:30 +0300

    tasks

---
+++ ExcelGenerator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            ///<summary>
+            ///The data is generated in the 'data' array.
+            ///</summary>
+
+            var people = new string[] { "Ivan", "Valentina", "George", "Ivelina", "Peter", "Nevena", "Mitko", "Pepi", "Sasho", "Lili" };
+            var cols = 3;
+            var rows = 3;
+            var generator = new Random();
+
+            var data = new object[cols, rows];
+
+            for (int i = 0; i < cols; i++)
+            {
+                data[i, 0] = people[generator.Next(people.Length)];
+                data[i, 1] = generator.Next(20, 81);
+                data[i, 2] = generator.Next(101);
+            }
+
+            ///<summary>
+            ///The excel app is created and filled with the info from the 'data' array, excel formulas and styles are applied.
+            ///</summary>
+
+            Excel.Application app = new Excel.Application();
+
+            object misValue = System.Reflection.Missing.Value;
+            Excel.Workbooks workbooks = app.Workbooks;
+            Excel.Workbook workbook = workbooks.Add(misValue);
+            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets.get_Item(1);
+
+            worksheet.Cells[1, 1] = "Name";
+            worksheet.Cells[1, 2] = "Age";
+            worksheet.Cells[1, 3] = "Score";
+            worksheet.Cells[1, 4] = "Average Score";
+
+            var writeRange = worksheet.Range[(Excel.Range)worksheet.Cells[2, 1], (Excel.Range)worksheet.Cells[cols + 1, rows]];
+            writeRange.Value2 = data;
+
+            worksheet.Cells[2, rows + 1].Formula = String.Format("=AVERAGE(C2:C{0})", cols + 1);
+
+            Excel.Range formatRange;
+            formatRange = worksheet.get_Range("A1", "D1");
+            formatRange.Font.Bold = "true";
+            formatRange.Interior.Color = Excel.XlRgbColor.rgbLightBlue;
+            formatRange = worksheet.get_Range("A2", "D" + cols);
+            Excel.FormatCondition format = formatRange.Rows.FormatConditions.Add(Type: Excel.XlFormatConditionType.xlExpression, Operator: Excel.XlFormatConditionOperator.xlGreaterEqual, Formula1: "=MOD(ROW();2) = 1");
+            format.Font.Color = Excel.XlRgbColor.rgbGreen;
+
+            workbook.SaveAs(AppDomain.CurrentDomain.BaseDirectory + "scores.xlsx", Excel.XlFileFormat.xlOpenXMLWorkbook, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            workbook.Close(true, misValue, misValue);
+            app.Quit();
+
+            Console.WriteLine("Excel file created , you can find the file in ExcelGenerator\\bin\\Debug");
+        }
+    }
+}
---
+++ FactorialsCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FactorialsCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = 10;
+
+            //Console.WriteLine(CalculateFactoriel(n));
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var repeat = 1000;
+            for (int i = 0; i < repeat; i++)
+            {
+                CalculateFactoriel(n);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+
+        static int CalculateFactoriel(int n)
+        {
+            var result = 1;
+
+            if (n > 1)
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    result *= i;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
